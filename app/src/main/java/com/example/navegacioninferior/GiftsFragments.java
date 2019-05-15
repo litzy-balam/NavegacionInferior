c35f99e8c89edefc85e0c84070c1a25bdf085223
litzy <litzy.balam@cbtis72.edu.mx>
2019-05-14 21:56:19 -0500

    Agregado fragmentos

---
+++ app/src/main/java/com/example/navegacioninferior/GiftsFragments.java
@@ -0,0 +1,29 @@
+package com.example.navegacioninferior;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class GiftsFragments extends Fragment {
+
+
+    public GiftsFragments() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_gifts_fragments, container, false);
+    }
+
+}
--- app/src/main/java/com/example/navegacioninferior/MainActivity.java
+++ app/src/main/java/com/example/navegacioninferior/MainActivity.java
@@ -6,12 +6,17 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.example.navegacioninferior.StoreFragment;
+import com.example.navegacioninferior.CartFragment;
+import com.example.navegacioninferior.GiftsFragments;
+import com.example.navegacioninferior.ProfileFragment;
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
@@ -36,29 +41,51 @@ public class MainActivity extends AppCompatActivity {
         });*/
         BottomNavigationView navigation=(BottomNavigationView)findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
+        //se agrega el load
         toolbar.setTitle("Shop");
+        loadFragment(new StoreFragment());
     }
+
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener=new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-           // Fragment fragment;
+           Fragment fragment;
             switch (item.getItemId()){
                 case R.id.navigation_shop:
                     toolbar.setTitle("Shop");
+                    //se agregan cambios
+                    fragment=new StoreFragment();
+                    loadFragment(fragment);
                     return true;
                 case R.id.navigation_gifts:
                     toolbar.setTitle("My Gifts");
+                    //se agregan cambios
+                    fragment=new GiftsFragments();
+                    loadFragment(fragment);
                     return true;
                 case R.id.navigation_cart:
                     toolbar.setTitle("Cart");
+                    //se agregan cambios
+                    fragment=new CartFragment();
+                    loadFragment(fragment);
                     return true;
                 case R.id.navigation_profile:
                     toolbar.setTitle("Profile");
+                    //se agregan cambios
+                    fragment=new ProfileFragment();
+                    loadFragment(fragment);
                     return true;
             }
             return false;
         }
     };
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container,fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
 }
