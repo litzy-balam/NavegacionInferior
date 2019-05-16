ac765363703473fb3e36a0424d1821cfcb4cd37a
litzy <litzy.balam@cbtis72.edu.mx>
2019-05-16 16:50:01 -0500

    Implementacion de shopFragment-Visualizacion de elementos en la cuadricula

---
+++ app/src/main/java/com/example/navegacioninferior/MyApplicacion.java
@@ -0,0 +1,49 @@
+package com.example.navegacioninferior;
+
+import android.app.Application;
+import android.text.TextUtils;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class MyApplicacion extends Application {
+
+    public static final String TAG=MyApplicacion.class.getSimpleName();
+    private RequestQueue mRequestQueue;
+    private static MyApplicacion mInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance=this;
+    }
+    public static synchronized MyApplicacion getInstance(){
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue(){
+        if(mRequestQueue==null){
+            mRequestQueue=Volley.newRequestQueue(getApplicationContext());
+        }
+
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req,String tag){
+        // set the default
+        req.setTag(TextUtils.isEmpty(tag)? TAG:tag);
+        getRequestQueue().add(req);
+    }
+    public <T> void addToRequestQueue(Request<T> req){
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
+
+
+    public void cancelPendingRequests(Object tag){
+        if(mRequestQueue !=null){
+            mRequestQueue.cancelAll(tag);
+        }
+    }
+
+}
